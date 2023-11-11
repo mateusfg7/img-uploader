e0f7a6115fd50cb9903f4a46ca262370e65ac042
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-11-10 22:22:09 -0300

    feat(ui): create main interfaces

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,114 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script lang="ts">
+  import { ImagePlus, Image, X, Loader2 } from 'lucide-svelte';
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
+  // let imageString: string | undefined = 'https://github.com/mateusfg7.png';
+  let imageString: string | undefined;
+  let fileInput: HTMLInputElement;
+  let file: File | undefined;
+  let isUploading = false;
+
+  const acceptedFormats = ['.jpeg', '.jpg', '.png', '.gif', '.webp'];
+
+  function onFileSelected(
+    e: Event & {
+      currentTarget: EventTarget & HTMLInputElement;
+    }
+  ) {
+    if (!e.currentTarget.files) return;
+
+    file = e.currentTarget.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (e) => {
+      imageString = e.target?.result as string;
+    };
   }
-</style>
+
+  function formatBytes(bytes: number, decimals = 2) {
+    if (!+bytes) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+  }
+
+  const removeImage = () => {
+    imageString = undefined;
+    file = undefined;
+  };
+
+  function handleUpload() {
+    isUploading = true;
+  }
+</script>
+
+<div class="max-w-lg m-auto space-y-10">
+  <div class="text-center">
+    <h1 class="text-4xl font-bold">Imgur Image Uploader</h1>
+    <p class="text-neutral-500">Upload your image below and directly receive a link for it.</p>
+  </div>
+  <div
+    class="flex flex-col items-center justify-center w-full gap-6 p-6 border border-dashed rounded-xl border-neutral-500 bg-neutral-100/50"
+  >
+    {#if imageString}
+      <div class="flex flex-col items-center gap-3">
+        <p class="text-2xl">Ready to upload</p>
+        <img src={imageString} alt="Uploaded" class="h-64 rounded-xl" />
+        <div class="flex items-center gap-3">
+          <span class="flex items-center gap-1">
+            <Image size="20" />
+            <span>
+              {file?.name} ({formatBytes(Number(file?.size))})
+            </span>
+          </span>
+          <button on:click={removeImage} class="transition-colors hover:text-red-500"
+            ><X size="20" /></button
+          >
+        </div>
+      </div>
+      {#if isUploading}
+        <button
+          disabled
+          class="flex items-center justify-center w-1/2 gap-2 p-3 text-lg text-white transition-colors bg-blue-800 cursor-wait rounded-xl opacity-90"
+        >
+          <Loader2 class="animate-spin" />
+          Uploading
+        </button>
+      {:else}
+        <button
+          on:click={handleUpload}
+          class="flex items-center justify-center w-1/2 p-3 text-lg text-white transition-colors bg-blue-800/90 rounded-xl hover:bg-blue-800"
+        >
+          Upload
+        </button>
+      {/if}
+    {:else}
+      <div class="flex flex-col items-center justify-center gap-2">
+        <ImagePlus size="35" strokeWidth="1.5" />
+        <div class="text-center">
+          <p>
+            <button
+              class="font-bold text-blue-800 transition-colors cursor-pointer hover:text-blue-600"
+              on:click={() => {
+                fileInput.click();
+              }}>Upload a file</button
+            > or drag and drop
+          </p>
+          <p class="text-xs text-neutral-600">JPG, PNG, WEBP or GIF up to 10MB</p>
+        </div>
+      </div>
+    {/if}
+  </div>
+</div>
+
+<input
+  type="file"
+  accept={acceptedFormats.join(',')}
+  on:change={(e) => onFileSelected(e)}
+  bind:this={fileInput}
+  class="hidden"
+/>
